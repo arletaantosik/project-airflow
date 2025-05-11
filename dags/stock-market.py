0def3b27917a8d38942b3e20094ac3e376bd4d4e
arletaantosik <162155471+arletaantosik@users.noreply.github.com>
2025-05-12 00:54:07 +0200

    Update stock-market.py

--- dags/stock-market.py
+++ dags/stock-market.py
@@ -2,22 +2,24 @@ from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime
 
-from include.stock_market.tasks import _get_stock_prices
+from include.stock_market.tasks import _get_stock_prices, _store_prices
 
 SYMBOL = 'NVDA'
 
 @dag(
     start_date=datetime(2023, 1, 1),
     schedule='@daily',
-    catchup=False,
+    catchup=False, #no backfilling - nie uruchamia z daty z przeszlosci
     tags=['stock_market'],
 
 )
 def stock_market():
     
-    @task.sensor(poke_interval=30, timeout=300, mode='poke')
+    @task.sensor(poke_interval=30, timeout=300, mode='poke') #poke_intervals -> asks every 30 seconds if the condition was met, timeout - how long sensor will try, 
+    # mode = poke -> sensor is active all time, in "reschedule" is activated every 30 seconds, which the second option is less resource consuming, but cheaper
     def is_api_available() -> PokeReturnValue: #create connection in airflow api - connection type: http, https://query1.finance.yahoo.com/v8/finance/chart/
         import requests
         
@@ -34,8 +36,14 @@ def stock_market():
         op_kwargs={'url': '{{ ti.xcom_pull(task_ids="is_api_available") }}', 'symbol': SYMBOL} #dictionary with 2 parameters, url and symbol, which are used in def _get_stock_prices
         # ti.xcom_pull(task_ids="is_api_available" - templating, {{ evaluates only when runs
     )
+
+    store_prices = PythonOperator(
+        task_id='store_prices',
+        python_callable=_store_prices,
+        op_kwargs={'stock': '{{ ti.xcom_pull(task_ids="get_stock_prices") }}'} #only 1 parameters in function
+    )
     
-    is_api_available() >> get_stock_prices # to run task
+    is_api_available() >> get_stock_prices >> store_prices # to run task
         
 
 stock_market()
